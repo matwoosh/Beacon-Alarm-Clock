58aee9f1f0d955ed2de7c76f6952910342e35f40
ja <krystian.chmura@gmail.com>
2017-01-11 18:01:10 +0100

    New alarm base

---
+++ src/main/java/trikita/talalarmo/App.java
@@ -0,0 +1,57 @@
+package trikita.talalarmo;
+
+import android.app.Application;
+
+import com.estimote.sdk.EstimoteSDK;
+
+import trikita.anvil.Anvil;
+import trikita.jedux.Action;
+import trikita.jedux.Logger;
+import trikita.jedux.Store;
+import trikita.talalarmo.alarm.AlarmController;
+import trikita.talalarmo.alarm.PersistanceController;
+
+public class App extends Application {
+
+    private static App instance;
+
+    private Store<Action, State> store;
+
+    public static State dispatch(Action action) {
+        return instance.store.dispatch(action);
+    }
+
+    public static State getState() {
+        return instance.store.getState();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // TODO: put your App ID and App Token here
+        // You can get them by adding your app on https://cloud.estimote.com/#/apps
+        EstimoteSDK.initialize(getApplicationContext(), "<#App ID#>", "<#App Token#>");
+
+        // uncomment to enable debug-level logging
+        // it's usually only a good idea when troubleshooting issues with the Estimote SDK
+        // EstimoteSDK.enableDebugLogging(true);
+
+
+        App.instance = this;
+
+        PersistanceController persistanceController = new PersistanceController(this);
+        State initialState = persistanceController.getSavedState();
+        if (initialState == null) {
+            initialState = State.Default.build();
+        }
+
+        this.store = new Store<>(new State.Reducer(),
+                initialState,
+                new Logger<>("Talalarmo"),
+                persistanceController,
+                new AlarmController(this));
+
+        this.store.subscribe(Anvil::render);
+    }
+}
